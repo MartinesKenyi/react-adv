c1bf0e78d3b6da998c91bfacc029eaa3b11d3ebe
AMB <kenyi030400@gmail.com>
2022-04-09 19:57:44 -0500

    lazyload nested

---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,9 @@
+export const NoLazy = () => {
+    return (
+        <div>
+            <h1 style={{ color: 'red' }}>
+                NoLazy Component
+            </h1>
+        </div>
+    )
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { lazy, LazyExoticComponent } from "react";
-// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 
 type JSXElement = () => JSX.Element
 
@@ -10,27 +10,23 @@ interface Route {
     name: string
 }
 
-const lazy1 = lazy(() => import(/* webpackChunkName: "lazypage1" */ '../01-lazyload/pages/LazyPage1'));
-const lazy2 = lazy(() => import(/* webpackChunkName: "lazypage2" */ '../01-lazyload/pages/LazyPage2'));
-const lazy3 = lazy(() => import(/* webpackChunkName: "lazypage3" */ '../01-lazyload/pages/LazyPage3'));
+const lazyLayout = lazy(() => import(/* webpackChunkName: "lazyLayout" */ '../01-lazyload/layout/LazyLayout'));
+
+// const lazy1 = lazy(() => import(/* webpackChunkName: "lazypage1" */ '../01-lazyload/pages/LazyPage1'));
+// const lazy2 = lazy(() => import(/* webpackChunkName: "lazypage2" */ '../01-lazyload/pages/LazyPage2'));
+// const lazy3 = lazy(() => import(/* webpackChunkName: "lazypage3" */ '../01-lazyload/pages/LazyPage3'));
 
 export const routes: Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: lazy1,
-        name: 'Lazy 1'
+        path: '/lazyload/*',
+        to: '/lazyload/',
+        Component: lazyLayout,
+        name: 'LazyLayout'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: lazy2,
-        name: 'Lazy 2'
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: lazy3,
-        name: 'Lazy 3'
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
     }
 ];
